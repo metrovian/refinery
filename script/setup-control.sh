d26fc49f17aedf471d11f0e290b7a23375545d2a
metrovian <paramedoct@gmail.com>
2025-12-31 20:31:10 +0900

    FORMAT. [CTRL]

--- script/setup-control.sh
+++ script/setup-control.sh
@@ -4,7 +4,7 @@ pip install \
         grpcio-tools \
 
 python -m grpc_tools.protoc \
-    -I/app/control/proto \
-    --python_out=/app/control \
-    --grpc_python_out=/app/control \
-    /app/control/proto/pendulum.proto
+        -I/app/control/proto \
+        --python_out=/app/control \
+        --grpc_python_out=/app/control \
+        /app/control/proto/pendulum.proto
