c4255022f2b120ab7055dbbaae146d2fa51cd8ac
metrovian <paramedoct@gmail.com>
2025-12-31 19:35:15 +0900

    ADD. [SIM] - Docker 관련 스크립트 업로드

--- 3rdparty/setup-debian.sh
+++ 3rdparty/setup-debian.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-git submodule foreach --recursive "git reset --hard"
-git submodule foreach --recursive "git clean -fd"
-git submodule update --init --recursive
-sudo apt update
-sudo apt install \
+apt update
+apt install -y \
+        build-essential \
+        meson \
+        ninja-build \
+        pkg-config \
         protobuf-compiler \
         libprotobuf-dev \
         libgrpc++-dev \
