ecc838af681b6f8ed5fa21eda722423d9e38a110
metrovian <paramedoct@gmail.com>
2026-04-16 20:40:23 +0900

    ADD. [COMMON] - 바이너리 정제소 기본파일 업로드

---
+++ 3rdparty/setup-debian.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git submodule foreach --recursive "git reset --hard"
+git submodule foreach --recursive "git clean -fd"
+git submodule update --init --recursive
+sudo apt update
+sudo apt install \
+    nodejs \
+    npm
